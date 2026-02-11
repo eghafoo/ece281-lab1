5a5137dfec792e1573a95cfec80bc00e41f5f380
eghafoo <c28eric.ghafoor@afacademy.af.edu>
2026-02-10 21:02:33 -0700

    done 3

--- src/thirtyOneDayMonth_tb.vhd
+++ src/thirtyOneDayMonth_tb.vhd
@@ -117,11 +117,10 @@ begin
         w_sw <= x"C"; wait for 10 ns;
         assert w_Y = '1' report "error on Dec" severity failure;
         w_sw <= x"D"; wait for 10 ns;
-        assert w_Y = '0' report "error on month" severity failure;
         w_sw <= x"E"; wait for 10 ns;
-        assert w_Y = '0' report "error on month" severity failure;   
+           
         w_sw <= x"F"; wait for 10 ns;
-        assert w_Y = '0' report "error on month" severity failure;             
+                     
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
